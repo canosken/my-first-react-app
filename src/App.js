1cfca4de834576c87aefb8cccb1034b048fb2e32
canosken <canosken@gmail.com>
2020-04-28 16:09:12 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Title from "./components/Title";
+import Pokemon from "./components/Pokemon"
 
 function App() {
   return (
     <div className="App">
       <main>
-      <Title content="Some Simple Title" />
+        <Title content="Some example title" />
+        <Pokemon />
       </main>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to Codaisseur
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+  )
 }
 
 export default App;
